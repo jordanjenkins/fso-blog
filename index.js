67071cda0f22da5957dca6f27b9f6e728426afd8
jordanjenkins <jenkins.587@gmail.com>
2021-11-19 18:04:47 -0500

    added and linked up controller/blogs, added utils/config. Not yet being used

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 const Blog = require('./models/blog')
 require('dotenv').config()
 
@@ -13,23 +14,8 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
