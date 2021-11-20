351027e0bb9c3ffe8fd3c940174f56b667fde16a
jordanjenkins <jenkins.587@gmail.com>
2021-11-19 19:04:13 -0500

    4. 3, added list_helper dummy function, set up arch for testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,32 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'commonjs': true,
-		'es2021': true
-	},
-	'extends': 'eslint:recommended',
-	'parserOptions': {
-		'ecmaVersion': 13
-	},
-	'rules': {
-		'indent': [
-			'error',
-			2
-		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		],
-		'eqeqeq': 'error',
+  'env': {
+    'node': true,
+    'commonjs': true,
+    'es2021': true,
+    'jest': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 13
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
     'no-trailing-spaces': 'error',
     'object-curly-spacing': [
       'error', 'always'
@@ -34,5 +35,5 @@ module.exports = {
       'error', { 'before': true, 'after': true }
     ],
     'no-console': 0
-	}
+  }
 }
--- index.js
+++ index.js
@@ -1,23 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
-const Blog = require('./models/blog')
-require('dotenv').config()
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
+const server = http.createServer(app)
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
