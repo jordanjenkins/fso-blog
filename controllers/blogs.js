4e2ea455d0827ab3d983438ad8f1b94e8916a0a0
jordanjenkins <jenkins.587@gmail.com>
2021-11-26 22:05:39 -0500

    complete 4.22, add userExtractor middleware for blog post and deletes

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,7 +1,7 @@
-const jwt = require('jsonwebtoken')
+//const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
+//const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -24,13 +24,13 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const decodedToken = jwt.verify(request.token, process.env.JWT_SECRET)
-  if (!request.token || !decodedToken.id) {
+
+  if(!request.token) {
     return response.status(401)
       .json({ error: 'token missing or invalid' })
   }
-  const user = await User.findById(decodedToken.id)
-  console.log(user)
+  const user = request.user
+
   const blog = new Blog ({
     title: body.title,
     author: body.author,
@@ -41,36 +41,27 @@ blogsRouter.post('/', async (request, response) => {
 
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog._id)
-  console.log(user)
   await user.save({ runValidators:false })
 
   response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  const blogId = await request.params.id
-  const blog = await Blog.findById(blogId)
-
-  const token = request.token
-  console.log('token', token)
-  const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
-  console.log('Decoded Token', decodedToken)
-
-  if (!token || !decodedToken.id) {
+  if(!request.token) {
     return response.status(401)
       .json({ error: 'token missing or invalid' })
   }
+  const blogId = await request.params.id
+  const blogToDel = await Blog.findById(blogId)
+  const user = request.user
 
-  if (blog.user.toString() === decodedToken.id) {
+  if (blogToDel.user.toString() === user._id.toString()) {
     await Blog.findByIdAndRemove(blogId)
-    response.status(204).end()
-  } else {
-    return response.status(403)
-      .json({ error: 'Forbidden, invalid user' })
+    return response.status(204).end()
   }
+  return response.status(403)
+    .json({ error: 'Forbidden, invalid user' }).end()
 })
-
-
 
 blogsRouter.put('/:id', async (request, response) => {
   const body = request.body
